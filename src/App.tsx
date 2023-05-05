1b02bbadd48d6f33a8b3f25c764519ee475fda13
NkwamoMaeva <nkwamomaeva@gmail.com>
2023-05-05 15:12:34 +0200

    Design and responsive conpleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,73 @@
 import React from "react";
 import Searchbar from "./components/searchbar/Searchbar";
+import { Icon } from "@iconify/react";
 import "./App.scss";
+
+const menus = [
+  { icon: "ic:sharp-mode-of-travel", name: "Voyager", href: "/travel" },
+  { icon: "tabler:qrcode", name: "Billets", href: "/ticket" },
+  { icon: "ph:book-open-text", name: "Offres", href: "/offer" },
+  { icon: "prime:user", name: "Compte", href: "/account" },
+];
 
 function App() {
   return (
     <div className="app">
+      <div className="navbar">
+        <div className="logo">
+          <div className="logo-img">
+            <Icon
+              icon="fluent-emoji-high-contrast:bat"
+              color="#61dafb"
+              width="28"
+              height="28"
+            />
+            <div></div>
+          </div>
+          <div className="logo-text">
+            bat<span>bat</span>
+          </div>
+        </div>
+        {menus.map((menu) => (
+          <a className="navbar-item" href={menu.href}>
+            <Icon icon={menu.icon} color="white" width="24" height="24" />
+            <span>{menu.name}</span>
+          </a>
+        ))}
+      </div>
+
+      {/* Mobile bottom navbar */}
+      <div className="navbar-bottom">
+        {menus.map((menu) => (
+          <a className="navbar-item" href={menu.href}>
+            <Icon icon={menu.icon} color="white" width="20" height="20" />
+            <span>{menu.name}</span>
+          </a>
+        ))}
+      </div>
       <header>
-        <div className="search-box">
-          <h2>Recherchez vos voyages, trajets courts et bien plus encore...</h2>
+        <span>
+          Recherchez vos voyages, trajets courts et bien plus encore...
+        </span>
+      </header>
+
+      <div className="search-box">
+        <div id="blur"></div>
+        <div className="search-header">
+          <span className="search-header-title">Rechercher</span>
+          <span className="search-header-button">Annuler</span>
+        </div>
+
+        <div className="searchbar">
           <Searchbar></Searchbar>
         </div>
-      </header>
+      </div>
+      <div className="contain">
+        {Array.from(Array(20)).map(() => (
+          <div className="item"></div>
+        ))}
+      </div>
+      <footer></footer>
     </div>
   );
 }
--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -1,28 +1,38 @@
 import React from "react";
 import "./Searchbar.scss";
+import { Icon } from "@iconify/react";
 
 function Searchbar() {
+  const handleFocus = () => {
+    console.log(window.scrollY);
+    document.querySelector(".search-box")?.scrollIntoView({
+      behavior: "smooth",
+    });
+    document.body.style.overflow = "hidden";
+    document.getElementById("blur")!.style.display = "block";
+  };
+  const handleFocusOut = () => {
+    document.body.style.overflowY = "scroll";
+    document.getElementById("blur")!.style.display = "none";
+  };
+
   return (
     <div className="box">
       <div>
-        <input type="text" placeholder="Une destination, demande..." />
+        <input
+          type="text"
+          onFocus={handleFocus}
+          onBlur={handleFocusOut}
+          placeholder="Une destination, demande..."
+        />
       </div>
       <div className="search-button">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
+        <Icon
+          icon="heroicons-outline:search"
+          color="#282c34"
           width="24"
           height="24"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="none"
-            stroke="#282c34"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0-14 0m18 11l-6-6"
-          />
-        </svg>
+        />
       </div>
     </div>
   );
